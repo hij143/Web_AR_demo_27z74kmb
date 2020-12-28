f14f3d0a45c47fe959aafa875f764aaa957962b1
Mohammad Hafijul Islam <68704854+hij143@users.noreply.github.com>
2020-12-28 14:40:35 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-AFRAME.registerComponent('foo', {
-            schema: {type: 'int', default: 5},
-            init: function () {
-            alert("Welcome in_it")},
-            update: function () {
-            alert("Hello World from Update");
-            console.log('Hello, World!');}
-        });
+AFRAME.registerComponent('log', {
+  schema: {
+    event: {type: 'string', default: ''},
+    message: {type: 'string', default: 'Hello, World!'}
+  },
+
+  init: function () {
+    var self = this;
+    this.eventHandlerFn = function () { console.log(self.data.message); };
+  },
+
+  update: function (oldData) {
+    var data = this.data;
+    var el = this.el;
+
+    // `event` updated. Remove the previous event listener if it exists.
+    if (oldData.event && data.event !== oldData.event) {
+      el.removeEventListener(oldData.event, this.eventHandlerFn);
+    }
+
+    if (data.event) {
+      el.addEventListener(data.event, this.eventHandlerFn);
+    } else {
+      console.log(data.message);
+    }
+  }
+});
