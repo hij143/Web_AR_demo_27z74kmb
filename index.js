117471b0fa89d9013cb8ed95a0d179058c618815
Mohammad Hafijul Islam <68704854+hij143@users.noreply.github.com>
2020-12-28 15:32:01 +0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ AFRAME.registerComponent('log', {
   },
 
   init: function () {
-    alert("Welcome in_it");
+    alert("Welcome log in_it");
     var self = this;
     this.eventHandlerFn = function () { console.log(self.data.message); };
   },
